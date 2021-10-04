f7550a977ce99d1561075d45321ec6d010ebdfb7
one <87183780@qq.com>
2021-10-04 19:28:16 +0800

    feat: dynamic route

---
+++ src/routes/news/[id]/index.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+export async function load() {
+  return {
+    props: {
+      text: Math.random(),
+    },
+  }
+}
+</script>
+
+<script>
+import { goto } from '$app/navigation'
+export let text
+</script>
+
+<h1>{text}</h1>
+
+<button on:click={() => goto(`/news/${Math.floor(Math.random() * 100)}`)}>test</button>
